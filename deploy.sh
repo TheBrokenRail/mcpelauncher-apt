c4dc38227c0756100a6db1dac7aad21feffc252e
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-09-08 15:54:56 -0400

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+git config --global user.email $(git log -n 1 --pretty='format:%aE')
+git config --global user.name $(git log -n 1 --pretty='format:%aN')
+
+cd repo
+git init
+git add .
+git commit --quiet -m "Deploy to Github Pages"
+git push --force "https://${GITHUB_TOKEN}@github.com/TheBrokenRail/mcpelauncher.git" master:gh-pages
