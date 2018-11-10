639104a7f64bc6927ae69fa6342ef6ca9b4c6fb9
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-10 14:30:04 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ cd repo
 git init
 git add .
 git commit --quiet -m "Deploy to Github Pages [ci skip]"
-git push --force "https://${GITHUB_TOKEN}@github.com/TheBrokenRail/mcpelauncher.git" master:gh-pages
+git push --force "https://${GITHUB_TOKEN}@github.com/TheBrokenRail/mcpelauncher-apt.git" master:gh-pages
